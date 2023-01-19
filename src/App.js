7f7d8b79d495ac6e4c82455e073a6e37fe63e934
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-20 00:13:46 +0100

    create item by mapping data array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import AppHeader from "./AppHeader";
+import AppContent from "./AppContent";
 function App() {
   return (
     <div className="App">
       <header>
         <AppHeader/>
       </header>
+      <main>
+        <AppContent/>
+      </main>
 
     </div>
   );
--- src/AppContent.js
+++ src/AppContent.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Events from './data'
+import { useState } from 'react'
 const AppContent = () => {
+    const [people,SetPeople] = useState(Events);
   return (
-    <div>AppContent</div>
+    <div className='container d-flex justify-content-center flex-column align-items-center'>
+        {people.map(el=>{
+        return <div className='item col-5'>
+                    <img src={el.img}></img>
+                    <span>{el.nome}</span>
+                    
+                    <p>{el.stato}</p>
+            </div>
+    })}</div>
   )
 }
 
